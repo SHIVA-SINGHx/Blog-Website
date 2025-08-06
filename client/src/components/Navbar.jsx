a130af0eb1f51539c0481d912c8c012792b3b056
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-08-06 05:53:31 -0700

    add navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { assest } from '../assets/assest'
+
 
 const Navbar = () => {
   return (
-    <div>
-      this is the navbar??
+    <div className='w-full h-16 md:20 flex items-center justify-between'>
+        {/* logo */}
+      <div className='flex items-center gap-6'>
+        <img src={assest.logo} alt="" className='w-[110px] h-[110px]'/>    
+      </div>
+      {/* Mobile */}
+      <div>
+        <a href="">Home</a>
+        <a href="">Blog</a>
+        <a href="">Feature</a>
+        <a href="">Contact</a>
+
+      </div>
+      {/* Desktop */}
+      <div>
+
+      </div>
+
+
     </div>
   )
 }
