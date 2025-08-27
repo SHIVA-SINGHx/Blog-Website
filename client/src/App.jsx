0192ffcc83f8479e2a6c8e161080d68461dba1b2
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-08-27 10:00:00 -0700

    rm extra stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 
+
 const App = () => {
   return (
-  <Navbar/>
+    <div className='px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64'>
+      <Navbar/>
+      
+
+    </div>
   )
 }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='bg-red-700'>
       this is the home page form react router dom
     </div>
   )
